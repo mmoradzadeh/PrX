708c53d1fd97dd246c8fe5b86a2f50139d4382d9
Mehdi Moradzadeh <morad97mm@gmail.com>
2025-04-11 19:22:21 -0400

    Added CS Windows Form Framework for an interview question and added git ignore

---
+++ CSharp/Clock Sample/GUITestApplication/FormTime.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace GUITestApplication
+{
+    partial class FormTime
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LabelTime
+            // 
+            this.LabelTime.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LabelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTime.Location = new System.Drawing.Point(0, 0);
+            this.LabelTime.Name = "LabelTime";
+            this.LabelTime.Size = new System.Drawing.Size(800, 450);
+            this.LabelTime.TabIndex = 0;
+            this.LabelTime.Text = "label1";
+            this.LabelTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormTime
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LabelTime);
+            this.MinimumSize = new System.Drawing.Size(200, 200);
+            this.Name = "FormTime";
+            this.Text = "Mehdi Moradzadeh Sample";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelTime;
+    }
+}
+
---
+++ CSharp/Clock Sample/GUITestApplication/FormTime.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUITestApplication
+{
+    public partial class FormTime : Form
+    {
+        public string CurrentTime
+        {
+            get { return LabelTime.Text; }
+            set
+            {
+                if (InvokeRequired)
+                    LabelTime.Invoke(new Action(() => LabelTime.Text = value));
+                else
+                    LabelTime.Text = value;
+            }
+        }
+
+        public FormTime()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var worker = new BackgroundWorker();
+            worker.WorkerSupportsCancellation = true;
+
+            worker.DoWork += Worker_DoWork1;
+
+            worker.RunWorkerAsync();
+        }
+
+        private void Worker_DoWork1(object sender, DoWorkEventArgs e)
+        {
+            var worker = (BackgroundWorker)sender;
+
+            while (!worker.CancellationPending)
+            {
+                CurrentTime = DateTime.Now.ToString("T");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
