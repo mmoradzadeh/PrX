a6384e7c1c969c2623b85b4114487a9694885c8c
Mehdi Moradzadeh <morad97mm@gmail.com>
2025-04-14 22:48:24 -0400

    Added setup sql files for all project to run test cases

---
+++ SQL/01-RecyclableAndLowFatProducts/setup.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Products (
+    product_id INT PRIMARY KEY,
+    low_fats ENUM('Y', 'N'),
+    recyclable ENUM('Y', 'N')
+);
+
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES
+(0, 'Y', 'N'),
+(1, 'Y', 'Y'),
+(2, 'N', 'Y'),
+(3, 'Y', 'Y'),
+(4, 'N', 'N');
---
+++ SQL/02-FindCustomerReferee/setup.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Customer (
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    referee_id INT
+);
+
+INSERT INTO Customer (id, name, referee_id) VALUES
+(1, 'Will', NULL),
+(2, 'Jane', NULL),
+(3, 'Alex', 2),
+(4, 'Bill', NULL),
+(5, 'Zack', 1),
+(6, 'Mark', 2);
